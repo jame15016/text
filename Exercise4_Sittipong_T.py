db154c170806b464278072b36e07ff313a21db33
jame15016 <78028492+jame15016@users.noreply.github.com>
2021-01-27 21:25:36 +0700

    Exercise4

---
+++ Exercise4_Sittipong_T.py
@@ -0,0 +1,15 @@
+Escore = 88.8
+GBscore = 66.2
+ICSscore = 77.2
+CPscore = 99.9
+
+print("--- Your Score---")
+print()
+print("Foundation English :", FEscore)
+print()
+print("General Business :", GBscore)
+print()
+print("Introduction to Computer System :", ICSscore)
+print()
+print("Computer Programming :", CPscore)
+print()
